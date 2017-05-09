b7b066ce5bff1970c318794422326d2ed24925bf
umarfarisi <pharese@gmail.com>
2017-05-09 22:57:26 +0700

    create BaseScreen

---
+++ app/src/main/java/isshukan/com/bukatender/screen/BaseScreen.java
@@ -0,0 +1,11 @@
+package isshukan.com.bukatender.screen;
+
+/**
+ * @author Muhammad Umar Farisi
+ * @created 09/05/2017
+ */
+public interface BaseScreen {
+    void loadViews();
+    void setDefaultSetting();
+    boolean isControllerNotNull();
+}
