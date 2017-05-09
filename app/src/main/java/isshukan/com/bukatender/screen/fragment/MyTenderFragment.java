78aea02d231aa15316dacf8cebe77b395f7588ad
umarfarisi <pharese@gmail.com>
2017-05-09 22:58:58 +0700

    create fragment and controller for MyTender, Tender, MyBid

---
+++ app/src/main/java/isshukan/com/bukatender/screen/fragment/MyTenderFragment.java
@@ -0,0 +1,45 @@
+package isshukan.com.bukatender.screen.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import isshukan.com.bukatender.R;
+import isshukan.com.bukatender.screen.BaseScreen;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MyTenderFragment extends Fragment implements BaseScreen{
+
+
+    public MyTenderFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_tender, container, false);
+    }
+
+    @Override
+    public void loadViews() {
+
+    }
+
+    @Override
+    public void setDefaultSetting() {
+
+    }
+
+    @Override
+    public boolean isControllerNotNull() {
+        return false;
+    }
+}
---
+++ app/src/main/java/isshukan/com/bukatender/screen/fragment/controller/MyTenderController.java
@@ -0,0 +1,8 @@
+package isshukan.com.bukatender.screen.fragment.controller;
+
+/**
+ * @author Muhammad Umar Farisi
+ * @created 09/05/2017
+ */
+public class MyTenderController {
+}
