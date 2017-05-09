6dd64bc757f4b944e8014471fd3b6626e310f128
umarfarisi <pharese@gmail.com>
2017-05-09 22:48:15 +0700

    create model User

---
+++ app/src/main/java/isshukan/com/bukatender/mode/User.java
@@ -0,0 +1,88 @@
+package isshukan.com.bukatender.mode;
+
+/**
+ * @author Muhammad Umar Farisi
+ * @created 09/05/2017
+ */
+public class User {
+
+    private String name;
+    private String email;
+    private String phone;
+    private String username;
+    private String password;
+
+    public User(String name, String email, String phone, String username, String password) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return username.equals(user.username);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return username.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
