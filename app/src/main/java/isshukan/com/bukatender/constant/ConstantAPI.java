5c166707ce71e05b7765f6a60f54119c9b0eac72
umarfarisi <pharese@gmail.com>
2017-05-09 22:48:35 +0700

    create ConstantAPI

---
+++ app/src/main/java/isshukan/com/bukatender/constant/ConstantAPI.java
@@ -0,0 +1,10 @@
+package isshukan.com.bukatender.constant;
+
+/**
+ * @author Muhammad Umar Farisi
+ * @created 09/05/2017
+ */
+public class ConstantAPI {
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+}
