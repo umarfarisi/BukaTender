a8a9a0d9610ff661bcdecae0ec944bd48826457f
umarfarisi <pharese@gmail.com>
2017-05-24 18:59:20 +0700

    create class Bid, BidDA, TagDA

---
+++ app/src/main/java/isshukan/com/bukatender/dataaccess/api/BidDA.java
@@ -0,0 +1,25 @@
+package isshukan.com.bukatender.dataaccess.api;
+
+import com.android.volley.Response;
+
+import isshukan.com.bukatender.model.Bid;
+
+/**
+ * @author Muhammad Umar Farisi
+ * @created 24/05/2017
+ */
+public class BidDA {
+
+    public void getProductBid(Response.Listener<String> listener, Response.ErrorListener errorListener){
+
+    }
+
+    public void getUserBid(Response.Listener<String> listener, Response.ErrorListener errorListener){
+
+    }
+
+    public void createBid(Bid bid, Response.Listener<String> listener, Response.ErrorListener errorListener){
+
+    }
+
+}
---
+++ app/src/main/java/isshukan/com/bukatender/dataaccess/api/TagDA.java
@@ -0,0 +1,8 @@
+package isshukan.com.bukatender.dataaccess.api;
+
+/**
+ * @author Muhammad Umar Farisi
+ * @created 24/05/2017
+ */
+public class TagDA {
+}
