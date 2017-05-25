dd2c42bdc7ef6aaf9253db289e18a0009febdbf5
umarfarisi <pharese@gmail.com>
2017-05-25 10:39:40 +0700

    create SetTender page

---
+++ app/src/main/java/isshukan/com/bukatender/screen/activity/SetTenderActivity.java
@@ -0,0 +1,63 @@
+package isshukan.com.bukatender.screen.activity;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import isshukan.com.bukatender.R;
+import isshukan.com.bukatender.screen.activity.controller.SetTenderController;
+
+public class SetTenderActivity extends BaseActivity implements View.OnClickListener{
+
+    private TextView titleTextView;
+    private TextView startingPriceTextView;
+    private TextView shortDescriptionTextView;
+    private Button validityPeriodButton;
+    private Button imageResourceButton;
+    private Button actionButton;
+
+    private SetTenderController controller;
+
+    @Override
+    public void loadViews() {
+        setContentView(R.layout.activity_set_tender);
+        titleTextView = (TextView) findViewById(R.id.titleTextView);
+        startingPriceTextView = (TextView) findViewById(R.id.startingPriceTextView);
+        shortDescriptionTextView = (TextView) findViewById(R.id.shortDescriptionTextView);
+        validityPeriodButton = (Button) findViewById(R.id.validityPeriodButton);
+        imageResourceButton = (Button) findViewById(R.id.imageResourceButton);
+        actionButton = (Button) findViewById(R.id.actionButton);
+    }
+
+    @Override
+    public void setDefaultSetting() {
+        validityPeriodButton.setOnClickListener(this);
+        imageResourceButton.setOnClickListener(this);
+        actionButton.setOnClickListener(this);
+        controller = new SetTenderController(this);
+    }
+
+    public TextView getTitleTextView() {
+        return titleTextView;
+    }
+
+    public TextView getStartingPriceTextView() {
+        return startingPriceTextView;
+    }
+
+    public TextView getShortDescriptionTextView() {
+        return shortDescriptionTextView;
+    }
+
+    @Override
+    public boolean isControllerNotNull() {
+        return controller != null;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(controller != null){
+            controller.onClick(v.getId());
+        }
+    }
+}
---
+++ app/src/main/java/isshukan/com/bukatender/screen/activity/controller/SetTenderController.java
@@ -0,0 +1,65 @@
+package isshukan.com.bukatender.screen.activity.controller;
+
+import android.content.Intent;
+
+import isshukan.com.bukatender.R;
+import isshukan.com.bukatender.constant.Constant;
+import isshukan.com.bukatender.model.Tender;
+import isshukan.com.bukatender.screen.activity.SetTenderActivity;
+
+/**
+ * @author Muhammad Umar Farisi
+ * @created 25/05/2017
+ */
+public class SetTenderController {
+
+    private SetTenderActivity activity;
+
+    private Tender oldTender;
+    private Tender newTender;
+
+    public SetTenderController(SetTenderActivity activity) {
+        this.activity = activity;
+        loadIntent();
+        loadData();
+    }
+
+    private void loadData() {
+        if(isSaveMode()){
+            activity.getTitleTextView().setHint(oldTender.getTitle());
+            activity.getStartingPriceTextView().setText(String.valueOf(oldTender.getStartingPrice()));
+            activity.getShortDescriptionTextView().setText(oldTender.getShortDescription());
+            newTender = new Tender(oldTender.getTenderId(), oldTender.getUserId(), oldTender.getTitle(), oldTender.getValidityPeriod(), oldTender.getStartingPrice(), oldTender.getImageResource(), oldTender.getShortDescription());
+        }else{
+            newTender = new Tender(-1, null, null, 0, 0, null, null);
+        }
+    }
+
+    private void loadIntent() {
+        Intent intent = activity.getIntent();
+        oldTender = (Tender) intent.getSerializableExtra(Constant.TENDER);
+    }
+
+    public void onClick(int id) {
+        switch (id){
+            case R.id.validityPeriodButton:
+                //TODO
+                break;
+            case R.id.imageResourceButton:
+                //TODO
+                break;
+            case R.id.actionButton:
+                //TODO
+                newTender.setTitle(activity.getTitleTextView().getText().toString());
+                newTender.setStartingPrice(Double.valueOf(activity.getStartingPriceTextView().getText().toString()));
+                newTender.setShortDescription(activity.getShortDescriptionTextView().getText().toString());
+                break;
+        }
+    }
+
+    private boolean isSaveMode(){
+        //tidak ada oldTender yg dipassing lewat intent yg artinya buat oldTender baru
+        return oldTender == null;
+    }
+
+}
